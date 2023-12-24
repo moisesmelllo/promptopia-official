7111af0f75328522095a34a406eb8a748bdee752
Moises Melo Viana Ferreira <moisesmvferreira@gmail.com>
2023-12-24 17:33:25 -0300

    feat: middleware to protect profile page

---
+++ src/middleware.ts
@@ -0,0 +1,25 @@
+import { NextResponse } from 'next/server'
+import type { NextRequest } from 'next/server'
+ 
+// This function can be marked `async` if using `await` inside
+export function middleware(request: NextRequest) {
+  const path = request.nextUrl.pathname
+
+  const privatePath = path === '/profile'
+
+  const token = request.cookies.get('next-auth.session-token')?.value || ''
+  console.log(token)
+  
+  if (privatePath && !token) {
+    return NextResponse.redirect(new URL('/', request.nextUrl))
+  }
+
+}
+
+// See "Matching Paths" below to learn more
+export const config = {
+  matcher:[
+    '/',
+    '/profile',
+  ]
+}
